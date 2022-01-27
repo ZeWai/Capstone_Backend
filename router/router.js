746eff76ea08e655e4d51b281dce67bba9d8e563
fankilky <91205739+fankilky@users.noreply.github.com>
2022-01-27 18:48:14 +0800

    DB updated

--- router/router.js
+++ router/router.js
@@ -1,57 +1,58 @@
 const express = require("express");
 
 class Router {
-    constructor(users, auth) {
-        this.users = users,
-            this.auth = auth;
-    }
+  constructor(users, auth) {
+    (this.users = users), (this.auth = auth);
+  }
 
-    router() {
-        let router = express.Router();
+  router() {
+    let router = express.Router();
 
-        //signup user
-        router.post('/signup', this.signup.bind(this))
-        //login
-        router.post("/login", this.login.bind(this))
-        //get user info
-        router.get('/users', this.usersAll.bind(this))
-        router.get('/users/:name', this.usersSigle.bind(this))
+    //signup user
+    router.post("/signup", this.signup.bind(this));
+    //login
+    router.post("/login", this.login.bind(this));
+    //get user info
+    router.get("/users", this.usersAll.bind(this));
+    router.get("/users/:name", this.usersSigle.bind(this));
 
-        return router;
-    }
+    return router;
+  }
 
-    signup(req, res) {
-        return this.users
-            .signup(
-                req.body.username,
-                req.body.email,
-                req.body.password,
-                req.body.postCode,
-                req.body.tel,
-                req.body.role,
-                req.body.name,
-                req.body.address,
-                req.body.icon,
-                req.body.image,
-                req.body.area,
-                req.body.size
-            )
-            .then((data) => res.json(data))
-    }
+  signup(req, res) {
+    return this.users
+      .signup(
+        req.body.username,
+        req.body.email,
+        req.body.password,
+        req.body.postCode,
+        req.body.tel,
+        req.body.role,
+        req.body.name,
+        req.body.address,
+        req.body.icon,
+        req.body.image,
+        req.body.area,
+        req.body.size
+      )
+      .then((data) => res.json(data));
+  }
 
-    login(req, res) {
-        return this.users
-            .login(req.body.username, req.body.password)
-            .then((token) => (token ? res.json(token) : res.sendStatus(401)));
-    }
+  login(req, res) {
+    return this.users
+      .login(req.body.username, req.body.password)
+      .then((token) => (token ? res.json(token) : res.sendStatus(401)));
+  }
 
-    usersAll(req, res) {
-        return this.users.usersAll().then((data) => res.json(data))
-    }
+  usersAll(req, res) {
+    return this.users.usersAll().then((data) => res.json(data));
+  }
 
-    usersSigle(req, res) {
-        return this.users.usersSigle(req.params.name).then((data) => res.json(data))
-    }
+  usersSigle(req, res) {
+    return this.users
+      .usersSigle(req.params.name)
+      .then((data) => res.json(data));
+  }
 }
 
 module.exports = Router;
