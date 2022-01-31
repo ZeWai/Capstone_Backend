85660c470530573d5b92414610d58ae060d39515
martinho <kit.hmk@gmail.com>
2022-01-31 16:32:53 +0800

    mas

--- router/Dashboardrouter.js
+++ router/Dashboardrouter.js
@@ -11,6 +11,9 @@ class DashboardRouter {
         router.get("/harvest", this.getharvest.bind(this));
         router.get("/growing", this.getgrowing.bind(this));
         router.get("/sow", this.getsow.bind(this));
+        router.get("/productivity", this.getproductivity.bind(this));
+        router.get("/progress", this.getprogress.bind(this));
+        router.get("/progressS/:id", this.getprogressS.bind(this));
         return router;
     }
 
@@ -36,7 +39,7 @@ getgrowing(req, res){
     return this.dashboardService
       .count2(1)
       .then((data) => {
-          console.log("atdashroutethen", data)
+          console.log("atdashroutethen2", data)
         res.json(data);
       })
       .catch((err) => {
@@ -49,7 +52,7 @@ getsow(req, res){
     return this.dashboardService
         .count3(1)
         .then((data) => {
-            console.log("atdashroutethen", data)
+            console.log("atdashroutethen3", data)
         res.json(data);
         })
         .catch((err) => {
@@ -57,6 +60,48 @@ getsow(req, res){
         return res.json(err);
         });
     }
+
+getproductivity(req, res){
+    console.log("at dash router4")
+    return this.dashboardService
+        .count4(1)
+        .then((data) => {
+            console.log("atdashroutethen4", data)
+        res.json(data);
+        })
+        .catch((err) => {
+        res.status(500);
+        return res.json(err);
+        });
+}
+
+getprogress(req, res){
+    console.log("at dash router5")
+    return this.dashboardService
+        .count5(1)
+        .then((data) => {
+            console.log("atdashroutethen5", data)
+        res.json(data);
+        })
+        .catch((err) => {
+        res.status(500);
+        return res.json(err);
+        });
+}
+
+getprogressS(req, res){
+    console.log("at dash router6",req.params.id)
+    return this.dashboardService
+        .count6(req.params.id)
+        .then((data) => {
+            console.log("atdashroutethen6", data)
+        res.json(data);
+        })
+        .catch((err) => {
+        res.status(500);
+        return res.json(err);
+        });
+}
 }
 
 module.exports = DashboardRouter;
--- service/Dashboardservice.js
+++ service/Dashboardservice.js
@@ -38,7 +38,7 @@ class DashboardService {
     }
 
     async count3(id){
-        console.log("at dashservice",id)
+        console.log("at dashsowservice",id)
         return await this.knex("crop")
         .select("name")
         .where("zone_id", id)
@@ -48,7 +48,58 @@ class DashboardService {
             if(info.length>0){
                 return info.length;
             } else {
-                throw new Error("not existing");
+                return 0;
+            }
+        })
+    }
+
+    async count4(id){
+        console.log("at dashproductservice",id)
+        return await this.knex("crop")
+        .select("name","yield","contribution", "harvest_date") 
+        .join("zone_crop","crop.id",'zone_crop.crop_id')
+        .where("crop.zone_id", id)
+        .andWhere("harvest", true)
+        .then((info)=> {
+            console.log("xxxx",info)
+            if(info.length>0){
+                return info;
+            } else {
+                console.log("not work")
+                return 0;
+            }
+        })
+    }
+    async count5(id){
+        console.log("at dashprogressservice", id)
+        return await this.knex("crop")
+        .select("id", "name","type","contribution") 
+        .where("zone_id", id)
+        .andWhere("harvest", false)
+        .then((info)=> {
+            console.log("yyyy",info)
+            if(info.length>0){
+                return info;
+            } else {
+                console.log("not work")
+                return 0;
+            }
+        })
+    }
+
+    async count6(id){
+        console.log("at dashprogressSservice", id)
+        return await this.knex("crop")
+        .select("crop.id", "name","type","contribution","sowing_date") 
+        .join("zone_crop","crop.id", 'zone_crop.crop_id')
+        .where("crop.id", id)
+        .then((info)=> {
+            console.log("yyyy",info)
+            if(info.length>0){
+                return info;
+            } else {
+                console.log("not work")
+                return 0;
             }
         })
     }
