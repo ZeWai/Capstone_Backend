f9d0ef7eaea8544f5ab05233c7c0238d2935e957
fankilky <91205739+fankilky@users.noreply.github.com>
2022-02-08 01:54:05 +0800

    update farmlog database

--- migrations/09_grooming.js
+++ migrations/09_grooming.js
@@ -4,15 +4,12 @@ exports.up = function (knex) {
     table.integer("farm_log_id").unsigned().unique();
     table.foreign("farm_log_id").references("farm_log.id");
     table.boolean("s4q1");
-    table.string("s4q1_substanceType");
-    table.string("s4q1_cropType");
+    table.string("s4q1_pest");
     table.string("s4q1_dosage");
     table.string("s4q1_remarks");
     table.boolean("s4q2");
-    table.string("s4q2_cropType");
     table.string("s4q2_remarks");
     table.boolean("s4q3");
-    table.string("s4q3_cropType");
     table.string("s4q3_remarks");
     table.string("s4q4");
     table.timestamps(false, true);
